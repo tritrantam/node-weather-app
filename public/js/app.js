055089e30866af5c66c3e3477dddf4f261fb89d3
tritran <tritran@r2integrated.com>
2021-07-11 13:55:07 +0700

    Setup App for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,8 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value;
     messageOne.textContent = 'Getting Weather Data';
     messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+
+    fetch('/weather?address='+ location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
